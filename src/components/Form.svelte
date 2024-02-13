20998521636d4680f2f699518c75fa47617a8652
Konrad Kubczyk <konrad@kubczyk.com>
2024-02-13 22:14:47 +0100

    style: consistent code formatting

--- src/components/Form.svelte
+++ src/components/Form.svelte
@@ -1,282 +1,281 @@
 <script>
-  import pdfMake from "pdfmake/build/pdfmake";
-  import htmlToPdfmake from "html-to-pdfmake";
+    import pdfMake from "pdfmake/build/pdfmake";
+    import htmlToPdfmake from "html-to-pdfmake";
 
-  pdfMake.fonts = {
-    // download default Roboto font from cdnjs.com
-    Roboto: {
-      normal:
-        "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
-      bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
-      italics:
-        "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
-      bolditalics:
-        "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
-    },
-  };
+    pdfMake.fonts = {
+        // download default Roboto font from cdnjs.com
+        Roboto: {
+            normal:
+                "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
+            bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
+            italics:
+                "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
+            bolditalics:
+                "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
+        },
+    };
 
-  let selectedFile;
-  let fileContent;
-  let table;
-  let reportSpan;
-  let clientName;
-  let totalAmount;
-  let displayedFields = {
-    Project: "Projekt",
-    Description: "Opis",
-    "End date": "Data",
-    Duration: "Czas",
-  }; // Declare displayedFields as a global variable
-  let hourlyRate = localStorage.getItem("hourlyRate") || 0;
+    let selectedFile;
+    let fileContent;
+    let table;
+    let reportSpan;
+    let clientName;
+    let totalAmount;
+    let displayedFields = {
+        Project: "Projekt",
+        Description: "Opis",
+        "End date": "Data",
+        Duration: "Czas",
+    }; // Declare displayedFields as a global variable
+    let hourlyRate = localStorage.getItem("hourlyRate") || 0;
 
-  function handleFileInput(event) {
-    totalAmount = 0;
-    selectedFile = event.target.files[0];
+    function handleFileInput(event) {
+        totalAmount = 0;
+        selectedFile = event.target.files[0];
 
-    if (selectedFile) {
-      const reader = new FileReader();
+        if (selectedFile) {
+            const reader = new FileReader();
 
-      reader.onload = () => {
-        fileContent = reader.result;
-        const rows = fileContent.split("\n");
-        const tableData = rows.map((row) => {
-          const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
-          const cells = row.split(regex);
-          return cells.map((cell) => {
-            if (cell.startsWith('"') && cell.endsWith('"')) {
-              return cell.slice(1, -1);
-            }
-            return cell;
-          });
-        });
+            reader.onload = () => {
+                fileContent = reader.result;
+                const rows = fileContent.split("\n");
+                const tableData = rows.map((row) => {
+                    const regex = /,(?=(?:(?:[^"]*"){2})*[^"]*$)/;
+                    const cells = row.split(regex);
+                    return cells.map((cell) => {
+                        if (cell.startsWith('"') && cell.endsWith('"')) {
+                            return cell.slice(1, -1);
+                        }
+                        return cell;
+                    });
+                });
 
-        // Remove empty rows
-        tableData.forEach((row, rowIndex) => {
-          if (row.length === 1 && row[0] === "") {
-            tableData.splice(rowIndex, 1);
-          }
-        });
+                // Remove empty rows
+                tableData.forEach((row, rowIndex) => {
+                    if (row.length === 1 && row[0] === "") {
+                        tableData.splice(rowIndex, 1);
+                    }
+                });
 
-        // Get client name
-        clientName = tableData[1][tableData[0].indexOf("Client")];
+                // Get client name
+                clientName = tableData[1][tableData[0].indexOf("Client")];
 
-        // Ensure that there is only one client in the file
-        tableData.forEach((row, rowIndex) => {
-          if (
-            rowIndex !== 0 &&
-            row[tableData[0].indexOf("Client")] !== clientName
-          ) {
-            alert("Error: Multiple clients found in the file");
-            selectedFile = null;
+                // Ensure that there is only one client in the file
+                tableData.forEach((row, rowIndex) => {
+                    if (
+                        rowIndex !== 0 &&
+                        row[tableData[0].indexOf("Client")] !== clientName
+                    ) {
+                        alert("Error: Multiple clients found in the file");
+                        selectedFile = null;
+                    }
+                });
+
+                // Filter the table data based on displayed fields
+                const filteredTableData = tableData.map((row) =>
+                    row.filter((_, columnIndex) =>
+                        Object.keys(displayedFields).includes(
+                            tableData[0][columnIndex].trim()
+                        )
+                    )
+                );
+
+                // Get first and last End date
+                const firstEndDate =
+                    filteredTableData[1][
+                        Object.keys(displayedFields).indexOf("End date")
+                        ];
+                const lastEndDate =
+                    filteredTableData[filteredTableData.length - 1][
+                        Object.keys(displayedFields).indexOf("End date")
+                        ];
+
+                // Generate report range based on first and last End date
+                reportSpan = `${firstEndDate.replaceAll(
+                    "-",
+                    "/"
+                )} - ${lastEndDate.replaceAll("-", "/")}`;
+
+                // Assign the filtered table data to a variable for display
+                table = filteredTableData;
+
+                // Change headers to match displayed fields
+                table[0] = table[0].map((header) => displayedFields[header.trim()]);
+
+                // Calculate amounts and insert them into the table
+                table.map((row, rowIndex) => {
+                    if (rowIndex === 0) {
+                        row.push("Wartość");
+                    } else {
+                        const duration =
+                            row[Object.keys(displayedFields).indexOf("Duration")];
+
+                        // Convert hh:mm:ss to decimal hours
+                        const hours = duration.split(":")[0];
+                        const minutes = duration.split(":")[1];
+                        const seconds = duration.split(":")[2];
+                        const decimalHours =
+                            Number(hours) + Number(minutes) / 60 + Number(seconds) / 3600;
+
+                        // Increase total amount
+                        totalAmount += decimalHours * hourlyRate;
+
+                        // Round and add amount to row
+                        row.push(Math.round(decimalHours * hourlyRate * 100) / 100);
+                    }
+                });
+
+                calculateValues();
+            };
+
+            reader.readAsText(selectedFile);
+        }
+    }
+
+    function handleHourlyRateInput(event) {
+        hourlyRate = event.target.value;
+        calculateValues();
+
+        // Update the table if it exists
+        if (table) {
+            table = [...table];
+        }
+
+        // Save the hourly rate to local storage
+        localStorage.setItem("hourlyRate", hourlyRate);
+    }
+
+    function calculateValues() {
+        // Check if table exists
+        if (!table) {
             return;
-          }
-        });
-
-        // Filter the table data based on displayed fields
-        const filteredTableData = tableData.map((row) =>
-          row.filter((_, columnIndex) =>
-            Object.keys(displayedFields).includes(
-              tableData[0][columnIndex].trim()
-            )
-          )
-        );
-
-        // Get first and last End date
-        const firstEndDate =
-          filteredTableData[1][
-            Object.keys(displayedFields).indexOf("End date")
-          ];
-        const lastEndDate =
-          filteredTableData[filteredTableData.length - 1][
-            Object.keys(displayedFields).indexOf("End date")
-          ];
-
-        // Generate report range based on first and last End date
-        reportSpan = `${firstEndDate.replaceAll(
-          "-",
-          "/"
-        )} - ${lastEndDate.replaceAll("-", "/")}`;
-
-        // Assign the filtered table data to a variable for display
-        table = filteredTableData;
-
-        // Change headers to match displayed fields
-        table[0] = table[0].map((header) => displayedFields[header.trim()]);
+        }
 
         // Calculate amounts and insert them into the table
         table.map((row, rowIndex) => {
-          if (rowIndex === 0) {
-            row.push("Wartość");
-          } else {
-            const duration =
-              row[Object.keys(displayedFields).indexOf("Duration")];
+            if (rowIndex !== 0) {
+                const duration = row[Object.keys(displayedFields).indexOf("Duration")];
+                const hours = duration.split(":")[0];
+                const minutes = duration.split(":")[1];
+                const seconds = duration.split(":")[2];
+                const decimalHours =
+                    Number(hours) + Number(minutes) / 60 + Number(seconds) / 3600;
 
-            // Convert hh:mm:ss to decimal hours
-            const hours = duration.split(":")[0];
-            const minutes = duration.split(":")[1];
-            const seconds = duration.split(":")[2];
-            const decimalHours =
-              Number(hours) + Number(minutes) / 60 + Number(seconds) / 3600;
-
-            // Increase total amount
-            totalAmount += decimalHours * hourlyRate;
-
-            // Round and add amount to row
-            row.push(Math.round(decimalHours * hourlyRate * 100) / 100);
-          }
+                row[row.length - 1] = Math.round(decimalHours * hourlyRate * 100) / 100;
+            }
         });
 
-        calculateValues();
-      };
-
-      reader.readAsText(selectedFile);
-    }
-  }
-
-  function handleHourlyRateInput(event) {
-    hourlyRate = event.target.value;
-    calculateValues();
-
-    // Update the table if it exists
-    if (table) {
-      table = [...table];
+        // Calculate total amount
+        totalAmount = table
+            .slice(1)
+            .reduce((sum, row) => sum + row[row.length - 1], 0);
     }
 
-    // Save the hourly rate to local storage
-    localStorage.setItem("hourlyRate", hourlyRate);
-  }
+    function generatePDF() {
+        // Get #report element
+        const report = document.getElementById("report");
 
-  function calculateValues() {
-    // Check if table exists
-    if (!table) {
-      return;
+        // Convert #report element to pdfmake-compatible object
+        const reportObject = htmlToPdfmake(report.innerHTML);
+
+        // Add title to reportObject
+        reportObject.unshift({
+            text: "Raport",
+            style: "header",
+            margin: [0, 0, 0, 10],
+            fontSize: 20,
+        });
+
+        // Create PDF document with name
+        pdfMake
+            .createPdf({
+                content: [reportObject],
+            })
+            .download(
+                `${clientName} - ${reportSpan
+                    .replaceAll("/", ".")
+                    .replaceAll(" ", "")}.pdf`
+            );
     }
-
-    // Calculate amounts and insert them into the table
-    table.map((row, rowIndex) => {
-      if (rowIndex !== 0) {
-        const duration = row[Object.keys(displayedFields).indexOf("Duration")];
-        const hours = duration.split(":")[0];
-        const minutes = duration.split(":")[1];
-        const seconds = duration.split(":")[2];
-        const decimalHours =
-          Number(hours) + Number(minutes) / 60 + Number(seconds) / 3600;
-
-        row[row.length - 1] = Math.round(decimalHours * hourlyRate * 100) / 100;
-      }
-    });
-
-    // Calculate total amount
-    totalAmount = table
-      .slice(1)
-      .reduce((sum, row) => sum + row[row.length - 1], 0);
-  }
-
-  function generatePDF() {
-    // Get #report element
-    const report = document.getElementById("report");
-
-    // Convert #report element to pdfmake-compatible object
-    const reportObject = htmlToPdfmake(report.innerHTML);
-
-    // Add title to reportObject
-    reportObject.unshift({
-      text: "Raport",
-      style: "header",
-      margin: [0, 0, 0, 10],
-      fontSize: 20,
-    });
-
-    // Create PDF document with name
-    pdfMake
-      .createPdf({
-        content: [reportObject],
-      })
-      .download(
-        `${clientName} - ${reportSpan
-          .replaceAll("/", ".")
-          .replaceAll(" ", "")}.pdf`
-      );
-  }
 </script>
 
 <div>
-  <form class="flex gap-5 flex-col md:flex-row">
-    <input
-      type="file"
-      class="file-input w-full outline outline-gray-300 focus:outline-gray-500 outline-2 outline-offset-2"
-      accept=".csv"
-      on:change={handleFileInput}
-    />
-    <input
-      type="number"
-      class="input w-full md:max-w-xs outline outline-gray-300 focus:outline-gray-500 outline-2 outline-offset-2"
-      placeholder="Stawka"
-      value={hourlyRate}
-      on:input={handleHourlyRateInput}
-    />
-  </form>
+    <form class="flex gap-5 flex-col md:flex-row">
+        <input
+                type="file"
+                class="file-input w-full outline outline-gray-300 focus:outline-gray-500 outline-2 outline-offset-2"
+                accept=".csv"
+                on:change={handleFileInput}
+        />
+        <input
+                type="number"
+                class="input w-full md:max-w-xs outline outline-gray-300 focus:outline-gray-500 outline-2 outline-offset-2"
+                placeholder="Stawka"
+                value={hourlyRate}
+                on:input={handleHourlyRateInput}
+        />
+    </form>
 </div>
 
 {#if selectedFile && table}
-  <div class="flex items-center mt-10 mb-2 gap-5">
-    <h2 class="text-xl">Raport</h2>
-    <div class="divider flex-grow" />
-    <button class="btn" on:click={generatePDF}>PDF</button>
-  </div>
-
-  <div id="report">
-    <div class="mb-5">
-      <p>
-        Kontrahent: <strong>{clientName}</strong>
-        <br />
-        Okres: <strong>{reportSpan}</strong>
-        <br />
-        Stawka: <strong>{hourlyRate} zł</strong>
-      </p>
+    <div class="flex items-center mt-10 mb-2 gap-5">
+        <h2 class="text-xl">Raport</h2>
+        <div class="divider flex-grow"/>
+        <button class="btn" on:click={generatePDF}>PDF</button>
     </div>
 
-    <div class="overflow-auto">
-      <table class="border-collapse w-full">
-        <thead>
-          <tr>
-            {#each table[0] as header}
-              <th class="px-4 py-2 border-2">{header}</th>
-            {/each}
-          </tr>
-        </thead>
-        <tbody>
-          {#each table.slice(1) as row}
-            <tr>
-              {#each row as cell, columnIndex}
-                {#if columnIndex === row.length - 1}
-                  <td class="px-4 py-2 border-2" style="text-align: right;">
-                    {parseFloat(cell).toFixed(2)}&nbsp;zł
-                  </td>
-                {:else}
-                  <td class="px-4 py-2 border-2">
-                    {cell}
-                  </td>
-                {/if}
-              {/each}
-            </tr>
-          {/each}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td class="px-4 py-2 border-2" colspan={table[0].length - 1}>
-              <strong>Suma</strong>
-            </td>
-            <td class="px-4 py-2 border-2">
-              <strong
-                >{(Math.round(totalAmount * 100) / 100).toFixed(
-                  2
-                )}&nbsp;zł</strong
-              >
-            </td>
-          </tr>
-        </tfoot>
-      </table>
+    <div id="report">
+        <div class="mb-5">
+            <p>
+                Kontrahent: <strong>{clientName}</strong>
+                <br/>
+                Okres: <strong>{reportSpan}</strong>
+                <br/>
+                Stawka: <strong>{hourlyRate} zł</strong>
+            </p>
+        </div>
+
+        <div class="overflow-auto">
+            <table class="border-collapse w-full">
+                <thead>
+                <tr>
+                    {#each table[0] as header}
+                        <th class="px-4 py-2 border-2">{header}</th>
+                    {/each}
+                </tr>
+                </thead>
+                <tbody>
+                {#each table.slice(1) as row}
+                    <tr>
+                        {#each row as cell, columnIndex}
+                            {#if columnIndex === row.length - 1}
+                                <td class="px-4 py-2 border-2" style="text-align: right;">
+                                    {parseFloat(cell).toFixed(2)}&nbsp;zł
+                                </td>
+                            {:else}
+                                <td class="px-4 py-2 border-2">
+                                    {cell}
+                                </td>
+                            {/if}
+                        {/each}
+                    </tr>
+                {/each}
+                </tbody>
+                <tfoot>
+                <tr>
+                    <td class="px-4 py-2 border-2" colspan={table[0].length - 1}>
+                        <strong>Suma</strong>
+                    </td>
+                    <td class="px-4 py-2 border-2">
+                        <strong
+                        >{(Math.round(totalAmount * 100) / 100).toFixed(
+                            2
+                        )}&nbsp;zł</strong
+                        >
+                    </td>
+                </tr>
+                </tfoot>
+            </table>
+        </div>
     </div>
-  </div>
 {/if}
